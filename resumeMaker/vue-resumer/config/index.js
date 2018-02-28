3b11109299fbbaca80c4efefb2fdf458bd363f21
Lee <326656669@qq.com>
2018-03-01 00:50:19 +0800

    修改路径问题 根目录/(文件夹/文件夹/)dist

--- resumeMaker/vue-resumer/config/index.js
+++ resumeMaker/vue-resumer/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/resumeMaker/vue-resumer/dist/',
+    assetsPublicPath: 'vue/resumeMaker/vue-resumer/dist/',
 
     /**
      * Source Maps
