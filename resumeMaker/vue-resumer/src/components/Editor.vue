f371dcd91f01f0527c7399183c35bf6ccc05bc0c
Lee <326656669@qq.com>
2018-03-01 19:34:07 +0800

    注册 EditorExperience EditorProfile 组件

--- resumeMaker/vue-resumer/src/components/Editor.vue
+++ resumeMaker/vue-resumer/src/components/Editor.vue
@@ -12,21 +12,54 @@
       </ol>
     </nav>
     <ol class="panes">
-      <li v-for="i in [0,1,2,3,4,5]"
-          v-bind:class="{active:currentTab === i}">
-        tab{{i+1}}
+      <li v-bind:class="{active:currentTab === 0}">
+        <EditorProfile v-bind:profile="profile" />
+      </li>
+      <li v-bind:class="{active:currentTab === 1}">
+        <EditorExperience v-bind:experience="experience"/>
+      </li>
+      <li v-bind:class="{active:currentTab === 2}">
+        <h2>项目经历</h2>
+      </li>
+      <li v-bind:class="{active:currentTab === 3}">
+        <h2>学习经历</h2>
+      </li>
+      <li v-bind:class="{active:currentTab === 4}">
+        <h2>获奖情况</h2>
+      </li>
+      <li v-bind:class="{active:currentTab === 5}">
+        <h2>联系方式</h2>
       </li>
     </ol>
   </div>
 </template>
 
 <script>
+  import EditorProfile from './EditorProfile'
+  import EditorExperience from './EditorExperience'
+
   export default {
+    components:{
+      EditorProfile,
+      EditorExperience
+    },
     data () {
       return {
         currentTab: 0,
-        icons:['shenfen','work0','heart','education','iconjiangbei','phone']
+        icons: ['shenfen', 'work0', 'heart', 'education', 'iconjiangbei', 'phone'],
+        profile: {
+          name: '',
+          gender: '',
+          birth: '',
+          city: ''
+        },
+        experience: [
+          {company: '', content: ''}
+        ]
       }
+    },
+    created(){},
+    methods: {
     }
   }
 </script>
@@ -57,8 +90,29 @@
       }
     }
     .panes {
+      width: 100%;
+      overflow: auto;
+      .container{
+        position: relative;
+        .el-icon-remove{
+          position: absolute;
+          right:0;
+          top:0;
+        }
+        .el-icon-circle-plus{
+          position: absolute;
+          top: 0;
+          right:0;
+        }
+      }
       > li {
         display: none;
+        width: 50%;
+        margin: 0 auto;
+        padding: 32px;
+        > h2 {
+          text-align: center;
+        }
         &.active {
           display: block;
         }
